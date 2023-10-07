c34d44c6c00b791a423e89fe237dd81406b17f82
Tanix <tanixlu@qq.com>
2023-10-07 21:00:02 +0800

    update

--- popup.js
+++ popup.js
@@ -3,17 +3,14 @@ const password_input = document.getElementById("password");
 const mail_input = document.getElementById("mail");
 const mail_password_input = document.getElementById("mail_password");
 
-chrome.storage.sync.get(["username", "password", "mail", "mail_password"], ({
-                                                                                username,
-                                                                                password,
-                                                                                mail,
-                                                                                mail_password
-                                                                            }) => {
-    username_input.value = username;
-    mail_input.value = mail;
-    password_input.value = password;
-    mail_password_input.value = mail_password
-});
+chrome.storage.sync.get(
+    ["username", "password", "mail", "mail_password"],
+    ({username, password, mail, mail_password}) => {
+        username_input.value = username;
+        password_input.value = password;
+        mail_input.value = mail;
+        mail_password_input.value = mail_password;
+    });
 
 // 任何输入改变都立即保存
 username_input.addEventListener("input", save);
