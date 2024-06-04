88d7547706bb34d6da68a842b1363e9ba3846269
Tanix <tanixlu@qq.com>
2024-06-04 14:15:04 +0800

    select mail suffix

--- mail_auto_login.js
+++ mail_auto_login.js
@@ -7,15 +7,21 @@ chrome.storage.sync.get(["mail_last_time"], ({mail_last_time}) => {
 
     const mail_input = document.getElementById("uid");
     const password_input = document.getElementById("password");
+    const remember_user_checkbox = document.querySelector("#saveUsername + i[class='checkbox']");
     const login_button = document.querySelector('button.u-btn.submit');
     chrome.storage.sync.get(["mail", "mail_password", "password"], ({mail, mail_password, password}) => {
-        mail_input.value = mail;
-        if (mail_password) {
-            password_input.value = mail_password;
-        } else {
-            password_input.value = password;
-        }
-        chrome.storage.sync.set({mail_last_time: Date.now()});
+        const [mail_name, mail_suffix] = mail.split("@");
+        // 输入邮箱用户名
+        mail_input.value = mail_name;
+        // 选择对应邮箱后缀
+        console.log(mail_suffix);
+        document.querySelector(`li[data-domain="${mail_suffix}"] > a`).click();
+        // 输入密码
+        password_input.value = mail_password ? mail_password : password;
+        // 记住密码
+        if (remember_user_checkbox) remember_user_checkbox.click();
+        const now = Date.now();
+        chrome.storage.sync.set({mail_last_time: now}).then(_ => console.log(`mail_last_time: ${now}`));
         login_button.click();
     });
 });
